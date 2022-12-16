d17311e396148de9d9200b60dc1a8019516f80e7
komasa <komasa72@ya.ru>
2022-12-17 01:20:45 +1000

    Finish 1st task of Course Project

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -8,25 +8,48 @@ const Users = () => {
         return users.map(user => (
             <tr>
                 <th scope="row" id={user._id}>{user.name}</th>
-                <td id={user.qualities._id}> {user.qualities.map((quality) => <span className={'badge m-1 bg-primary'}>{quality.name}</span>)} </td>
+                <td id={user.qualities._id}> {user.qualities.map((quality) => <span className={'badge m-1 bg-' + quality.color}>{quality.name}</span>)} </td>
                 <td id={user.profession._id}>{user.profession.name}</td>
                 <td>{user.completedMeetings}</td>
                 <td>{user.rate}</td>
-                <td id={user._id}><button className ="badge bg-danger p-2 border-0">Delete</button></td>
+                <td id={user._id}><button className ="badge bg-danger p-2 border-0" onClick={() => handleDelete(user)}>Delete</button></td>
             </tr>
         ))
     }
     const handleDelete = (userId) => {
-
+        setUsers(prevState => prevState.filter((user) => user !== userId))
     }
-    const renderPhrase = (number= 12) => {
-        return number !== 0 ? `${number} человек тусанет с тобой сегодня` : 'Никто с тобой не тусанет'
+    const renderPhrase = (number) => {
+        function declOfNum(number, text_forms) {
+            number = Math.abs(number) % 100;
+            const n1 = number % 10;
+            if (number > 10 && number < 20) {
+                return text_forms[2];
+            }
+            if (n1 > 1 && n1 < 5) {
+                return text_forms[1];
+            }
+            if (n1 === 1) {
+                return text_forms[0];
+            }
+            return text_forms[2];
+        }
+
+        const phrase = number !== 0 ? `${number} ${declOfNum(number, ["человек", "человека", "человек"])} ${declOfNum(number, ["тусанёт", "тусанёт", "тусанут"])} с тобой сегодня` : 'Никто с тобой не тусанет'
+
+        const colorPhrase = number !== 0 ? 'badge bg-primary' : 'badge bg-danger'
+
+        return  <span className = {colorPhrase} >
+                    {phrase}
+                </span>
     };
 
 
     return (
         <>
-        <h2><span className="badge bg-primary">{renderPhrase()}</span></h2>
+        <h2>
+            {renderPhrase(renderUserList().length)}
+        </h2>
         <table className="table">
         <thead>
         <tr>

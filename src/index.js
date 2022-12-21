f50396be52a194cabca1bd6975b7de52e4a31f3f
komasa <komasa72@ya.ru>
2022-12-22 01:31:55 +1000

    Refactor code 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Users from './app/components/users'
 import './index.css';
+import App from './app/app'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Users />
+    < App />
   </React.StrictMode>
 );
 

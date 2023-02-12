66482e87abebf2ce093027faa8d05dae3790e487
Konstantin <komasa72@ya.ru>
2023-02-12 16:41:46 +1000

    lesson Frorms 1.1

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../components/textField";
 const Login = () => {
+  const [data, setData] = useState({ email: "", password: "" });
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form action="">
+      <TextField
+        onChange={handleChange}
+        name={"email"}
+        label={"Электронная почта"}
+        type={"text"}
+        value={data.email}
+      />
+      <TextField
+        onChange={handleChange}
+        name={"email"}
+        label={"Электронная почта"}
+        type={"text"}
+        value={data.email}
+      />
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          value={data.email}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Пароль</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          value={data.password}
+        />
+      </div>
+      <button type="button">Войти</button>
+    </form>
   );
 };
 
